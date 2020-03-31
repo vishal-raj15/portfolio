e9427c1ec10798b686e847f2a8775281394bfa85
vishal-raj15 <vishalraj1380@gmail.com>
2020-03-31 19:02:24 +0530

    Updates

---
+++ precache-manifest.37ba0bd6a32981ac9b18d95fedc844f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb09fa706f65ed9db2a9897dd7b22f3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cde4d0a46f0946c3f98d",
+    "url": "/my-app/static/css/2.30f4e2e5.chunk.css"
+  },
+  {
+    "revision": "26492542308602429e68",
+    "url": "/my-app/static/css/main.7cf888b5.chunk.css"
+  },
+  {
+    "revision": "cde4d0a46f0946c3f98d",
+    "url": "/my-app/static/js/2.e6f4b37d.chunk.js"
+  },
+  {
+    "revision": "5c5999cc7d2072b7103aa20fdf9d0a4c",
+    "url": "/my-app/static/js/2.e6f4b37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26492542308602429e68",
+    "url": "/my-app/static/js/main.d530f5ed.chunk.js"
+  },
+  {
+    "revision": "883cd556cdfcda25bd63",
+    "url": "/my-app/static/js/runtime-main.fd1cb725.js"
+  }
+]);
