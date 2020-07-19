9cc920e3c873b56697647b4240d2b68e03cab3dd
vishal-raj15 <vishalraj1380@gmail.com>
2020-07-19 11:19:17 +0530

    mail-box working

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,61 @@
 import React, { Component} from 'react';
 import {Grid , Cell, List , ListItem,ListItemContent} from 'react-mdl';
-
+import axios from 'axios';
+import emailjs from 'emailjs-com';
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            name:"",
+            email:"",
+            message:""
+
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+
+
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleSubmit= (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm(
+                "gmail",
+                "vishmail",
+                ".contact-form" ,
+                "user_EWbIWk099ZUQuBQnxkB3h"
+            )
+            .then()
+            .catch();
+            this.setState({
+                name:"",
+                email:"",
+                message:""
+            });
+    };
+
+
+    // sendEmail(e) {
+    //     e.preventDefault();
+    
+    //     emailjs.sendForm("gmail",
+    //         "template_GipBbFcU",
+    //         ".contact-form" ,
+    //         "user_EWbIWk099ZUQuBQnxkB3h")
+
+    //       .then((result) => {
+    //           console.log(result.text);
+    //       }, (error) => {
+    //           console.log(error.text);
+    //       });
+    //   }
+   
     render() {
         return (
             <div className="topco" id="contact">
@@ -25,6 +78,7 @@ class Contact extends Component {
                            <div className="in2"><i class="fa fa-envelope" aria-hidden="true"></i>vishalraj3344@gmail.com</div>
                            
                           </div>
+
                         </div>
 
                         
@@ -35,17 +89,18 @@ class Contact extends Component {
                     <div className="right-bar">
             <div className="container p-3  text-white" >
             <div className="contact-body">
-            <form>
+
+            <form className="contact-form"  onSubmit={this.handleSubmit.bind(this)} >
             <div className="form-group">
-    <input type="name" className="form-control" id="exampleInputname1" aria-describedby="nameHelp" placeholder="abc"/>
+    <input type="name" name="name" className="form-control" id="name" aria-describedby="nameHelp" value={this.state.name} onChange={this.handleChange.bind(this)} placeholder="abc"/>
   </div>
   <br />
   <div className="form-group">
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="abc@gmail.com"/>
+    <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.handleChange.bind(this)} placeholder="abc@gmail.com"/>
   </div>
   <div class="form-group">
     <label for="exampleFormControlTextarea1"></label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder="message"></textarea>
+    <textarea class="form-control" name="message" id="message" rows="6" value={this.state.message} onChange={this.handleChange.bind(this)} placeholder="message"></textarea>
   </div>
   <br />
   <button type="submit" className="btn btn-info btn-lg btn-block">Send a message</button>
@@ -73,6 +128,7 @@ class Contact extends Component {
                             </a>
 
                         </div>
+
 
             </div>
         )
